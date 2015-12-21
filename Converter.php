0e6ac93db5dd1694e3af6c6ea47992cc08928c50
Александр <arkataev@gmail.com>
2015-12-21 23:50:58 +0300

    CSV Converter v.1.0

--- Converter.php
+++ Converter.php
@@ -2,5 +2,83 @@
 
 class Converter
 {
+	private $calls = [];
 
+	public function __constructor()
+	{	
+		
+	}
+
+	public function normalize($input)
+	{
+		if ($csv = fopen($input, 'r') === false) {
+        	throw new Exception('Неудалось открыть файл'."\n");
+        }
+        
+        $csv = fopen($input, 'r');
+        $rowCount = 0;
+		while ($row = fgetcsv($csv)){
+			$rowCount ++;
+			$validData = $this->validate($row, $rowCount);
+			if (!empty($validData[0])) {
+				$phone = '+7'.$validData[0];
+				$date = date('H:i:s d.m.Y', $validData[1]);
+				$this->calls[$phone] = $date;
+			} else {
+				throw new Exception('Неудалось прочитать данные'."\n");
+			}
+		}
+		return $this;
+	}
+
+	public function sortCallsByDate($order) 
+	{
+		$cmp = function($a, $b) use ($order) {
+			
+			if ($a == $b) { 
+				return 0; 
+			}
+
+			if ($order === 'ASC') { 
+				if ($a < $b) {
+					return -1;
+				} else {
+					return 1;
+				}
+			}
+
+			if ($order === 'DSC') { 
+				if ($a > $b) {
+					return -1;
+				} else {
+					return 1;
+				}
+			}
+		};
+		
+		if (uasort($this->calls, $cmp)) {
+			return $this->calls;
+		} else {
+			throw new Exception('Ошибка чтения данных'."\n");
+		}
+	}
+	
+	private function validate($data, $row)
+	{
+		if (strlen((string)$data[0]) == 10 && $this->isValidTimeStamp($data[1])) {
+			return $data;
+		} else { 
+			fwrite(STDERR, 'Ошибка в данных в строке: '.$row."\n");
+		}
+	}
+	
+	private function isValidTimeStamp($strTimestamp)
+	{
+	    return ((string) (int) 
+	    	$strTimestamp === $strTimestamp) 
+	        && ($strTimestamp <= PHP_INT_MAX)
+	        && ($strTimestamp >= ~PHP_INT_MAX);
+	}
+	
+	
 }
--- console.php
+++ console.php
@@ -1,3 +1,16 @@
 <?php
-
 require_once 'Converter.php';
+$converter = new Converter();
+
+try {
+	$csvNorm = $converter->normalize($argv[1])->sortCallsByDate($argv[2]);	
+} catch (Exception $e) {
+	if ($e) {
+		fwrite(STDERR, $e->getMessage());
+	}
+}
+
+foreach($csvNorm as $key => $value) {
+	$string = $key.' - '.$value.',';
+	fwrite(STDOUT, $string."\n");	
+}
